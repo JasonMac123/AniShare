8e80fdb336abbda55e18072b49d3931cfe44ea6d
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-16 18:09:57 -0400

    Creates array of items to iterate over for user items

--- app/components/usermenu/UserMenu.tsx
+++ app/components/usermenu/UserMenu.tsx
@@ -1,6 +1,27 @@
 import UserMenuLogo from "./UserMenuLogo";
 
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+
 const UserMenu = () => {
+  const userMenuItems = [
+    {
+      label: "Home",
+      href: "/",
+      icon: BsHouseFill,
+    },
+    {
+      label: "Notifications",
+      href: "/notifications",
+      icon: BsBellFill,
+    },
+    {
+      label: "Profile",
+      href: "/users/123",
+      icon: FaUser,
+    },
+  ];
+
   return (
     <div className="w-1/4 h-full pr-4 md:pr-6">
       <div className="flex flex-col items-end">
