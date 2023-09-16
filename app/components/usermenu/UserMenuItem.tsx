2bc0f3d672a7ee3ccd099e7c69dd4817328aeaeb
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-16 19:08:37 -0400

    Creates interface props for usermenu

--- app/components/usermenu/UserMenuItem.tsx
+++ app/components/usermenu/UserMenuItem.tsx
@@ -1,4 +1,18 @@
-const UserMenuItem = () => {
+import { IconType } from "react-icons";
+
+interface UserMenuItemProps {
+  label: string;
+  href?: string;
+  icon: IconType;
+  onClick?: () => void;
+}
+
+const UserMenuItem: React.FC<UserMenuItemProps> = ({
+  label,
+  href,
+  icon,
+  onClick,
+}) => {
   return <div></div>;
 };
 
