d7fcd960098e2df8a4562e578069e1cfc6511d46
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-16 18:07:00 -0400

    Adds redirection to mainpage on clicking logo

--- app/components/usermenu/UserMenuLogo.tsx
+++ app/components/usermenu/UserMenuLogo.tsx
@@ -6,7 +6,12 @@ const SideBarLogo = () => {
   const router = useRouter();
 
   return (
-    <div className="rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-red-400 hover:bg-opacity-10 cursor-pointer transition">
+    <div
+      className="rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-red-400 hover:bg-opacity-10 cursor-pointer transition"
+      onClick={() => {
+        router.push("/");
+      }}
+    >
       <GrMultimedia size={28} color="blue" />
     </div>
   );
