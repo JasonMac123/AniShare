85d8c6ea6e988d131da6a6ee53a855aecb9ef9a7
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-16 18:02:46 -0400

    Creates usermenu logo to show for all pages

---
+++ app/components/usermenu/UserMenuLogo.tsx
@@ -0,0 +1,15 @@
+import { useRouter } from "next/router";
+
+import { GrMultimedia } from "react-icons/gr";
+
+const SideBarLogo = () => {
+  const router = useRouter();
+
+  return (
+    <div className="rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-red-400 hover:bg-opacity-10 cursor-pointer transition">
+      <GrMultimedia />
+    </div>
+  );
+};
+
+export default SideBarLogo;
