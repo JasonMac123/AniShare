162462494f94c2381e9b792522f730cbb2780b89
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-16 19:12:28 -0400

    Adds usermenu item styling

--- app/components/usermenu/UserMenuItem.tsx
+++ app/components/usermenu/UserMenuItem.tsx
@@ -10,10 +10,20 @@ interface UserMenuItemProps {
 const UserMenuItem: React.FC<UserMenuItemProps> = ({
   label,
   href,
-  icon,
+  icon: Icon,
   onClick,
 }) => {
-  return <div></div>;
+  return (
+    <div className="flex flex-row items-center">
+      <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
+        <Icon size={28} color="blue" />
+      </div>
+      <div className="relative hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-slate-300 hover:bg-opacity-10 cursor-pointer">
+        <Icon size={24} color="blue" />
+        <p className="hidden lg:block text-blue-300 text-xl">{label}</p>
+      </div>
+    </div>
+  );
 };
 
 export default UserMenuItem;
