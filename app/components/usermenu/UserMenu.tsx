cb74ed5f34cc5e055a96195a9e84f1f5477596c5
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-15 20:52:20 -0400

    Adds title to user menu

--- app/components/usermenu/UserMenu.tsx
+++ app/components/usermenu/UserMenu.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 const UserMenu = () => {
-  return <div></div>;
+  return (
+    <div className="w-1/4 h-full">
+      <h1>AniShare</h1>
+    </div>
+  );
 };
 
 export default UserMenu;
