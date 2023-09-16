5d475916d5aba1393f76ff6d3319b3f2fea73239
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-16 18:03:01 -0400

    Adds logo to user menu

--- app/components/usermenu/UserMenu.tsx
+++ app/components/usermenu/UserMenu.tsx
@@ -1,9 +1,13 @@
-"use client";
+import UserMenuLogo from "./UserMenuLogo";
 
 const UserMenu = () => {
   return (
-    <div className="w-1/4 h-full">
-      <h1>AniShare</h1>
+    <div className="w-1/4 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <UserMenuLogo />
+        </div>
+      </div>
     </div>
   );
 };
