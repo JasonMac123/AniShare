846a858edafaf71e4e7a6e8f167b7a203120e061
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-17 23:14:24 -0400

    Adds similar styling to user menu items

--- app/components/usermenu/UserMenuCreate.tsx
+++ app/components/usermenu/UserMenuCreate.tsx
@@ -10,8 +10,9 @@ const UserMenuCreate = () => {
       <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-red-500 hover:bg-opacity-50 transition cursor-pointer">
         <BiCommentAdd size={28} color="red" />
       </div>
-      <div className="mt-6 hidden lg:block px-4 py-2 rounded-full bg-red-500 hover:bg-opacity-90 cursor-pointer transition">
-        <p className="hidden lg:block text-center font-semibold text-white text-[20px]">
+      <div className="mt-6 hidden lg:block px-4 py-2 rounded-full hover:bg-red-500 hover:bg-opacity-90 cursor-pointer transition">
+        <BiCommentAdd size={28} color="red" className="hover:text-red-500" />
+        <p className="hidden lg:block text-center font-semibold text-white hover:text-red-500 text-[20px]">
           Create
         </p>
       </div>
