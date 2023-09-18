f4cd266a2a22beec2d28c4df662553023deb51db
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-17 23:13:20 -0400

    Adds styling for desktop screens

--- app/components/usermenu/UserMenuCreate.tsx
+++ app/components/usermenu/UserMenuCreate.tsx
@@ -10,6 +10,11 @@ const UserMenuCreate = () => {
       <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-red-500 hover:bg-opacity-50 transition cursor-pointer">
         <BiCommentAdd size={28} color="red" />
       </div>
+      <div className="mt-6 hidden lg:block px-4 py-2 rounded-full bg-red-500 hover:bg-opacity-90 cursor-pointer transition">
+        <p className="hidden lg:block text-center font-semibold text-white text-[20px]">
+          Create
+        </p>
+      </div>
     </div>
   );
 };
