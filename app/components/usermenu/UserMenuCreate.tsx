62fbb41938002f401e48aa1e96f41646dabe2966
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-17 23:10:58 -0400

    Create usermenu create media button

--- app/components/usermenu/UserMenuCreate.tsx
+++ app/components/usermenu/UserMenuCreate.tsx
@@ -1,5 +1,17 @@
+import { useRouter } from "next/router";
+
+import { BiCommentAdd } from "react-icons/bi";
+
 const UserMenuCreate = () => {
-  return <div></div>;
+  const router = useRouter();
+
+  return (
+    <div onClick={() => router.push("/")}>
+      <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-red-500 hover:bg-opacity-50 transition cursor-pointer">
+        <BiCommentAdd size={28} color="red" />
+      </div>
+    </div>
+  );
 };
 
 export default UserMenuCreate;
