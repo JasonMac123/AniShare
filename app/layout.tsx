b240022d362a3c6f67e82c015f2bf9b9a152c3f6
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-17 23:24:43 -0400

    Adds homepage default to be black background for dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <UserMenu />
-        {children}
-        <FollowBar />
+        <div className="h-screen bg-black">
+          <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+            <div className="grid grid-cols-4 h-full">
+              <UserMenu />
+              <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
+                {children}
+              </div>
+              <FollowBar />
+            </div>
+          </div>
+        </div>
       </body>
     </html>
   );
