08988d6b0fe0c011c0e3e69030e94d2ac1d1e805
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-17 23:30:43 -0400

    Adds header information and redirection

--- app/components/Layout/Header.tsx
+++ app/components/Layout/Header.tsx
@@ -1,4 +1,18 @@
-const Header = () => {
+import { useRouter } from "next/router";
+import { useCallback } from "react";
+
+interface HeaderProps {
+  label: string;
+  showBackArrow?: boolean;
+}
+
+const Header: React.FC<HeaderProps> = ({ label, showBackArrow }) => {
+  const router = useRouter();
+
+  const handleBack = useCallback(() => {
+    router.back();
+  }, [router]);
+
   return <div></div>;
 };
 
