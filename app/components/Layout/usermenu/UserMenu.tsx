f5cbafdc7a0cbf3e4049e726a7f5c6e6a94fb9b5
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-18 21:18:35 -0400

    Removes random slate background

--- app/components/Layout/usermenu/UserMenu.tsx
+++ app/components/Layout/usermenu/UserMenu.tsx
@@ -31,7 +31,7 @@ const UserMenu = () => {
   ];
 
   return (
-    <div className="w-1/4 h-full pr-4 md:pr-6 bg-slate-300">
+    <div className="w-1/4 h-full pr-4 md:pr-6">
       <div className="flex flex-col items-end">
         <div className="space-y-2 lg:w-[230px]">
           <UserMenuLogo />
