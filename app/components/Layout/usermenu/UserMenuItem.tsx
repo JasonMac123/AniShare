f9fadf0aee328ccf300919cd29930d28e83a153b
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-18 21:54:46 -0400

    Fixes usermenu going off screen during mobile displays

--- app/components/Layout/usermenu/UserMenuItem.tsx
+++ app/components/Layout/usermenu/UserMenuItem.tsx
@@ -15,10 +15,10 @@ const UserMenuItem: React.FC<UserMenuItemProps> = ({
 }) => {
   return (
     <div className="flex flex-row items-center">
-      <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-darkWhite cursor-pointer lg:hidden">
+      <div className="rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-darkWhite cursor-pointer lg:hidden">
         <Icon size={28} color="red" />
       </div>
-      <div className="relative hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-darkWhite cursor-pointer">
+      <div className="hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-darkWhite cursor-pointer">
         <Icon size={24} color="red" />
         <p className="hidden lg:block text-brightRed text-xl">{label}</p>
       </div>
