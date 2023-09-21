ca799aa0c467e7f5356cfb6581aefd8a5c04b40d
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-20 23:20:51 -0400

    Adds props to modal component

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,4 +1,24 @@
-const Modal = () => {
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onOpen: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  onOpen,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+}) => {
   return <div></div>;
 };
 
