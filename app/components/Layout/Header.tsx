061b2e53a75f99d0944f915e69bc4c72f7e287d5
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-20 23:06:12 -0400

    Creates home component

--- app/components/Layout/Header.tsx
+++ app/components/Layout/Header.tsx
@@ -3,6 +3,8 @@
 import { useRouter } from "next/navigation";
 import { useCallback } from "react";
 
+import { BiArrowBack } from "react-icons/bi";
+
 interface HeaderProps {
   label: string;
 }
@@ -14,7 +16,17 @@ const Header: React.FC<HeaderProps> = ({ label }) => {
     router.back();
   }, [router]);
 
-  return <div></div>;
+  return (
+    <div className="p-5">
+      <div className="flex flex-row items-center gap-2 text-isabelline">
+        <BiArrowBack
+          onClick={handleBack}
+          className="cursor-pointer hover:opacity-90 transition"
+        />
+        <h1 className="text-xl font-semibold">{label}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
