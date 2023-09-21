ba1c3e90edac7aa4ee4f10ef0045a0380f054cb0
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-20 23:18:33 -0400

    Imports modal into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 
 import UserMenu from "./components/Layout/usermenu/UserMenu";
 import FollowBar from "./components/Layout/FollowBar";
+import Modal from "./components/Modal";
 
 export const metadata = {
   title: "AniShare",
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Modal />
         <div className="h-screen bg-davyGray">
           {/*Add light mode version */}
           <div className="grid grid-cols-4 h-full">
