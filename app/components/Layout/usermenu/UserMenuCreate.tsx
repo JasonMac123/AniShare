f54f2c897379d58b57cdc455ee36a138bc3d106a
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-20 22:59:50 -0400

    Removes animation for user menu create

--- app/components/Layout/usermenu/UserMenuCreate.tsx
+++ app/components/Layout/usermenu/UserMenuCreate.tsx
@@ -12,7 +12,7 @@ const UserMenuCreate = () => {
       <div className="lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-isabelline text-isabelline hover:text-taupe hover:bg-opacity-50 transition cursor-pointer">
         <BiCommentAdd size={28} />
       </div>
-      <div className="hidden lg:flex p-4 gap-4 rounded-full items-center hover:bg-isabelline text-isabelline hover:text-taupe cursor-pointer transition">
+      <div className="hidden lg:flex p-4 gap-4 rounded-full items-center hover:bg-isabelline text-isabelline hover:text-taupe cursor-pointer">
         <BiCommentAdd size={24} />
         <p className="hidden lg:block text-center font-semibold text-xl">
           Create
