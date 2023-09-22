f462eae16d5ef412bf7aeca26da062949ed16fb2
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-22 16:12:18 -0400

    Adds possibility to add icon to button

--- app/components/input/Button.tsx
+++ app/components/input/Button.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { IconType } from "react-icons";
+
 interface ButtonProps {
   label: string;
   secondary?: boolean;
@@ -6,6 +10,7 @@ interface ButtonProps {
   onClick: () => void;
   outline?: boolean;
   disabled?: boolean;
+  icon?: IconType;
 }
 
 const Button: React.FC<ButtonProps> = ({
@@ -16,12 +21,13 @@ const Button: React.FC<ButtonProps> = ({
   onClick,
   outline,
   disabled,
+  icon: Icon,
 }) => {
   return (
     <button
       disabled={disabled}
       onClick={onClick}
-      className={`disabled: opacity-70 disabled:cursor-not-allowed rounded-full font-semibold hover:opacity-80 transition border-2 
+      className={`relative disabled: opacity-70 disabled:cursor-not-allowed rounded-full font-semibold hover:opacity-80 transition border-2 
       ${fullWidth ? "w-full" : "w-fit"}
       ${large ? "text-xl px-5 py-3" : "text-md py-2 px-4"}
       ${outline ? "bg-transparent border-isabelline text-isabelline" : ""}
@@ -31,6 +37,7 @@ const Button: React.FC<ButtonProps> = ({
           : "bg-melon text-isabelline border-melon"
       }`}
     >
+      {Icon && <Icon size={24} className="absolute left-3 top-3" />}
       {label}
     </button>
   );
