2ef1fbe42fb45c346683bbcf5a62e1e6cd29cf8c
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-22 16:07:29 -0400

    Adds login modal content

--- app/components/modal/LoginModal.tsx
+++ app/components/modal/LoginModal.tsx
@@ -1,5 +1,66 @@
+"use client";
+import { useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+
+import Modal from "./Modal";
+import InputForm from "../input/InputForm";
+
+import useLogin from "../hooks/useLoginModal";
+
 const LoginModal = () => {
-  return <div></div>;
+  const loginModal = useLogin();
+  const [loading, setLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setLoading(true);
+  };
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <InputForm
+        id="email"
+        label="Email"
+        type="text"
+        disabled={loading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <InputForm
+        id="password"
+        label="Password"
+        type="password"
+        disabled={loading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={loading}
+      isOpen={loginModal.isOpen}
+      title="Login"
+      actionLabel="Continue"
+      onClose={loginModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+    />
+  );
 };
 
 export default LoginModal;
