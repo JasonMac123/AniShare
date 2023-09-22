5d17b286b830d46b6abc3d1a9aff934faa0262f8
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-22 15:41:42 -0400

    Adds label to input form

--- app/components/input/InputForm.tsx
+++ app/components/input/InputForm.tsx
@@ -4,6 +4,7 @@ import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 
 interface InputProps {
   id: string;
+  label: string;
   register: UseFormRegister<FieldValues>;
   errors: FieldErrors;
   placeholder?: string;
@@ -15,6 +16,7 @@ interface InputProps {
 
 const InputForm: React.FC<InputProps> = ({
   id,
+  label,
   errors,
   register,
   placeholder,
@@ -24,15 +26,27 @@ const InputForm: React.FC<InputProps> = ({
   onChange,
 }) => {
   return (
-    <input
-      id={id}
-      disabled={disabled}
-      onChange={onChange}
-      value={value}
-      placeholder={placeholder}
-      type={type}
-      className="w-full p-4 text-lg bg-silver border-2 border-darkGray rounded-md outline-none text-isabelline focus:border-melon focus:border-2 disabled:bg-red-300 disabled:opacity-70 disabled:cursor-not-allowed"
-    />
+    <div className="w-full relative">
+      <input
+        id={id}
+        disabled={disabled}
+        onChange={onChange}
+        value={value}
+        placeholder={placeholder}
+        type={type}
+        className={`w-full peer p-4 pt-6 text-lg bg-silver border-2 rounded-md outline-none text-isabelline focus:border-2 disabled:bg-red-300 disabled:opacity-70 disabled:cursor-not-allowed
+
+        ${errors[id] ? "border-rose-600" : "border-melon"} 
+        ${errors[id] ? "focus:border-rose-600" : "focus:border-black"}`}
+      />
+      <label
+        className={`absolute text-sm duration-150 transform -translate-y-3 top-5 z-10 origin-[0] 
+        ${errors[id] ? "text-rose-600" : ""}
+        peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4`}
+      >
+        {label}
+      </label>
+    </div>
   );
 };
 
