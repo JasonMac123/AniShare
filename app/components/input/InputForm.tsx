bf01ebc804ef1c4121a40fc1f1764f70749de6e5
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-23 17:37:41 -0400

    Fixes label position place outside of the input

--- app/components/input/InputForm.tsx
+++ app/components/input/InputForm.tsx
@@ -32,13 +32,13 @@ const InputForm: React.FC<InputProps> = ({
         {...register(id, { required })}
         placeholder=" "
         type={type}
-        className={`w-full peer p-4 pt-6 text-lg bg-silver border-2 rounded-md outline-none text-isabelline focus:border-2 disabled:bg-red-300 disabled:opacity-70 disabled:cursor-not-allowed
+        className={`w-full peer p-4 pt-6 text-lg bg-white border-2 rounded-md outline-none focus:border-2 disabled:bg-red-300 disabled:opacity-70 disabled:cursor-not-allowed
 
         ${errors[id] ? "border-rose-600" : "border-melon"} 
         ${errors[id] ? "focus:border-rose-600" : "focus:border-black"}`}
       />
       <label
-        className={`absolute text-sm duration-150 transform -translate-y-3 top-5 z-10 origin-[0] 
+        className={`absolute text-sm duration-150 transform -translate-y-3 top-5 z-10 origin-[0] text-black left-9
         ${errors[id] ? "text-rose-600" : ""}
         peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4`}
       >
