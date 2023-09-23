63bd7a3231f93ab028f7c2c3e1d0da19c3ca682b
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-23 18:00:09 -0400

    Changes colour of modal

--- app/components/modal/Modal.tsx
+++ app/components/modal/Modal.tsx
@@ -47,7 +47,7 @@ const Modal: React.FC<ModalProps> = ({
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-40 outline-none focus:outline-none bg-neutral-800 bg-opacity-70">
         <div className="relative w-full lg:w-3/7 my-6 lg:max-w-3xl h-full lg:h-auto z">
-          <div className="h-full lg:h-auto border-0 rounded-lg shadow-lg flex flex-col w-full relative bg-taupe z-50 outline-none focus:outline-none">
+          <div className="h-full lg:h-auto border-0 rounded-lg shadow-lg flex flex-col w-full relative bg-melon z-50 outline-none focus:outline-none">
             <div className="flex items-center justify-between p-10 rounded-t">
               <h3 className="text-3xl font-semibold text-isabelline">
                 {title}
