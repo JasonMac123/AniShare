038b58967d4d370e0a5cd6ad8984b89fcabc3a1e
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-23 17:46:10 -0400

    Creates registermodal component by resusing login modal

--- app/components/modal/RegisterModal.tsx
+++ app/components/modal/RegisterModal.tsx
@@ -1,5 +1,99 @@
+"use client";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "react-toastify";
+import { FcGoogle } from "react-icons/fc";
+
+import Modal from "./Modal";
+import InputForm from "../input/InputForm";
+import Button from "../input/Button";
+
+import useLogin from "../hooks/useLoginModal";
+import useRegister from "../hooks/useRegisterModal";
+
 const RegisterModal = () => {
-  return <div></div>;
+  const loginModal = useLogin();
+  const registerModal = useRegister();
+  const [loading, setLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const redirectRegister = useCallback(() => {
+    registerModal.onClose();
+    loginModal.onOpen();
+  }, [loginModal, registerModal]);
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setLoading(true);
+  };
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <InputForm
+        id="email"
+        label="Email"
+        type="text"
+        disabled={loading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <InputForm
+        id="password"
+        label="Password"
+        type="password"
+        disabled={loading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        secondary
+        fullWidth
+        large
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <div className="text-white flex-row flex gap-3">
+        <div>Already have an account?</div>
+        <div
+          onClick={redirectRegister}
+          className="cursor-pointer hover:underline hover:text-neutral-400"
+        >
+          Login here!
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={loading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Sign up"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
 };
 
 export default RegisterModal;
