f55226170142d1d064f9d823506284afd1f68b94
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-26 17:26:15 -0400

    Updated next auth to latest version

--- pages/api/auth/[...nextauth.].ts
+++ pages/api/auth/[...nextauth.].ts
@@ -1,52 +1,53 @@
-import bcrypt from "bcrypt";
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../app/prisma/prismadb";
+import bcrypt from "bcrypt";
 
-import prisma from "@/app/prisma/prismadb";
-
-export default NextAuth({
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       name: "credentials",
       credentials: {
         email: { label: "email", type: "text" },
-        password: { label: "password", type: "password" },
+        password: { label: "password", type: "text" },
       },
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) {
           throw new Error("Invalid credentials");
         }
+
         const user = await prisma.user.findUnique({
           where: {
             email: credentials.email,
           },
         });
 
-        if (!user || !user.hashedPassword) {
-          throw new Error("Invalid crendeitals");
+        if (!user || !user?.hashedPassword) {
+          throw new Error("Invalid credentials");
         }
 
-        const correctPassword = await bcrypt.compare(
+        const isCorrect = await bcrypt.compare(
           credentials.password,
           user.hashedPassword
         );
 
-        if (!correctPassword) {
-          throw new Error("Invalid credentials");
+        if (!isCorrect) {
+          throw new Error("Invalid Password");
         }
 
         return user;
       },
     }),
   ],
-  debug: process.env.NODE_ENV === "development",
+  pages: {
+    signIn: "/",
+  },
   session: {
     strategy: "jwt",
   },
-  jwt: {
-    secret: process.env.NEXTAUTH_JWT_SECRET,
-  },
   secret: process.env.NEXTAUTH_SECRET,
-});
+};
+
+export default NextAuth(authOptions);
