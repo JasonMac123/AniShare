b1c75458886dda2c1a595f455dce0efd25e4d48e
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-29 12:38:45 -0400

    Goes through array of users and displays them on followbar

--- app/components/layout/FollowBar.tsx
+++ app/components/layout/FollowBar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { safeUser } from "@/app/types";
+import Avatar from "../Avatar";
 
 interface FollowBarProps {
   users: safeUser[];
@@ -11,7 +12,15 @@ const FollowBar: React.FC<FollowBarProps> = ({ users }) => {
     <div className="px-6 py-4 hidden lg:block">
       <div className="bg-melon rounded-xl p-4">
         <h2 className="text-taupe text-xl font-semibold">Who to follow</h2>
-        <div className="flex flex-col gap-6 mt-4"></div>
+        <div className="flex flex-col gap-6 mt-4">
+          {users.map((user: safeUser) => {
+            return (
+              <div key={user.id} className="flex flex-row gap-4">
+                <Avatar />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
