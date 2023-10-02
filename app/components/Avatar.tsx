863a6e1e842b2570427b1a3346d09d3bca1da942
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-01 20:26:55 -0400

    Creates Avatar profile to read user picture and display

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -38,7 +38,16 @@ const Avatar: React.FC<AvatarProps> = ({
       ${hasBorder ? "border-4 border-black" : ""} 
       ${isLarge ? "h-32 w-32" : "h-12 w-12"} `}
     >
-      <Image />
+      <Image
+        fill
+        style={{
+          objectFit: "cover",
+          borderRadius: "100%",
+        }}
+        alt="Avatar"
+        onClick={handleClick}
+        src={user.image}
+      />
     </div>
   );
 };
