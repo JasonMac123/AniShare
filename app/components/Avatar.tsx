cfc308ff9afa5205cc457afc798cac2b92700e96
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-01 20:20:14 -0400

    Passes down user information into avatar to display

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import { safeUser } from "../types";
+
 interface AvatarProps {
   userId: string;
+  user: safeUser;
   isLarge?: boolean;
   hasBorder?: boolean;
 }
 
-const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
+const Avatar: React.FC<AvatarProps> = ({
+  userId,
+  isLarge,
+  hasBorder,
+  user,
+}) => {
   return <div></div>;
 };
 
