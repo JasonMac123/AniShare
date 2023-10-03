10b36512a9c30fd652006c5880bc3d881b5de731
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-02 20:40:08 -0400

    Adds disable back arrow for header feature

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,10 +6,11 @@ import { useCallback } from "react";
 import { BiArrowBack } from "react-icons/bi";
 
 interface HeaderProps {
+  disableBack: Boolean;
   label: string;
 }
 
-const Header: React.FC<HeaderProps> = ({ label }) => {
+const Header: React.FC<HeaderProps> = ({ label, disableBack }) => {
   const router = useRouter();
 
   const handleBack = useCallback(() => {
@@ -19,10 +20,12 @@ const Header: React.FC<HeaderProps> = ({ label }) => {
   return (
     <div className="p-5">
       <div className="flex flex-row items-center gap-2 text-isabelline">
-        <BiArrowBack
-          onClick={handleBack}
-          className="cursor-pointer hover:opacity-90 transition"
-        />
+        {disableBack && (
+          <BiArrowBack
+            onClick={handleBack}
+            className="cursor-pointer hover:opacity-90 transition"
+          />
+        )}
         <h1 className="text-xl font-semibold">{label}</h1>
       </div>
     </div>
