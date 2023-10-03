8987083f1faf959dbfeb64645eb1afd13db7b360
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-02 20:37:33 -0400

    Displays username on userpage

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,5 +1,21 @@
-const UserView = () => {
-  return <div></div>;
+import axios from "axios";
+
+import Header from "@/app/components/Header";
+
+interface UserViewPageProps {
+  userId?: string;
+}
+
+const UserView = async ({ params }: { params: UserViewPageProps }) => {
+  const userData = await axios.get(
+    `http://localhost:3000/api/user/${params.userId}`
+  );
+
+  return (
+    <>
+      <Header label={userData.data.username} />
+    </>
+  );
 };
 
 export default UserView;
