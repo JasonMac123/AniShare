f96d6a7d2e0568781ed6642055ac990bf0a45551
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-04 22:03:12 -0400

    Imports user bio component and passes current user data and user page date

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -2,12 +2,15 @@ import axios from "axios";
 
 import Header from "@/app/components/Header";
 import UserCover from "@/app/components/user/UserCover";
+import getCurrentUser from "@/app/functions/getCurrentUser";
+import UserBio from "@/app/components/user/UserBio";
 
 interface UserViewPageProps {
   userId?: string;
 }
 
 const UserView = async ({ params }: { params: UserViewPageProps }) => {
+  const currrentUser = await getCurrentUser();
   const userData = await axios.get(
     `http://localhost:3000/api/user/${params.userId}`
   );
@@ -16,6 +19,10 @@ const UserView = async ({ params }: { params: UserViewPageProps }) => {
     <>
       <Header label={userData.data.username} />
       <UserCover userId={userData.data.id} user={userData.data} />
+      <UserBio
+        currentUser={currrentUser?.id}
+        createdAt={userData.data.createdAt}
+      />
     </>
   );
 };
