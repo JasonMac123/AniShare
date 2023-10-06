2c1f73838f93de99e138fb1fd38f0470f6bb644b
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-05 23:58:43 -0400

    Creates editmodal submit api function and form state hooks

--- app/components/modal/EditModal.tsx
+++ app/components/modal/EditModal.tsx
@@ -1,4 +1,52 @@
-const Editmodal = () => {
+"use client";
+
+import { useState } from "react";
+import { SubmitHandler, FieldValues, useForm } from "react-hook-form";
+
+import useEdit from "../hooks/useEditModal";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+interface EditModalProps {
+  userId?: string;
+}
+
+const Editmodal: React.FC<EditModalProps> = ({ userId }) => {
+  const editModal = useEdit();
+  const [loading, setLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      username: "",
+      bio: "",
+      coverImage: "",
+      profileImage: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setLoading(true);
+
+    axios
+      .patch("/api/edit", {
+        ...data,
+        id: userId,
+      })
+      .then(() => {
+        editModal.onClose();
+      })
+      .catch((error) => {
+        toast("Something went wrong");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return <div></div>;
 };
 
