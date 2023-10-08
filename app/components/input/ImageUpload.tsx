c9181deb5b012c89b9b06340a3b12b23a5be0200
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-07 22:03:42 -0400

    Fixes typo with usedropzone

--- app/components/input/ImageUpload.tsx
+++ app/components/input/ImageUpload.tsx
@@ -52,7 +52,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           "w-full p-4 text-white text-center border-2 border-dotted rounded-md border-neutral-700",
       })}
     >
-      <input {...getInputProps} />
+      <input {...getInputProps()} />
       {value ? (
         <div className="flex items-center justify-center">
           <Image src={value} height={100} width={100} alt="Uploaded Image" />
