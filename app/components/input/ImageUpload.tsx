1842e4f2f484ecf76513b5034be75f2aa921534a
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-07 22:04:58 -0400

    Adds additional styling to image upload

--- app/components/input/ImageUpload.tsx
+++ app/components/input/ImageUpload.tsx
@@ -49,7 +49,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
     <div
       {...getRootProps({
         className:
-          "w-full p-4 text-white text-center border-2 border-dotted rounded-md border-neutral-700",
+          "w-full p-4 text-white text-center border-2 border-dotted rounded-md border-neutral-700 cursor-pointer bg-white",
       })}
     >
       <input {...getInputProps()} />
