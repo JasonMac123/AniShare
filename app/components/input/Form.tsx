e6e8886c340e9920b2d38b950fe4e039cf1da7be
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-09 23:36:31 -0400

    Create submit function to handle post creation

--- app/components/input/Form.tsx
+++ app/components/input/Form.tsx
@@ -1,5 +1,18 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+import getCurrentUser from "@/app/functions/getCurrentUser";
+import useLogin from "../hooks/useLoginModal";
+import useRegister from "../hooks/useRegisterModal";
+
+import InputForm from "./InputForm";
+
 interface FormProps {
   placeholder: string;
   isComment?: boolean;
@@ -7,7 +20,53 @@ interface FormProps {
 }
 
 const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
-  return <div></div>;
+  const loginModal = useLogin();
+  const registerModal = useRegister();
+
+  const user = getCurrentUser();
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      body: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setLoading(true);
+
+    axios
+      .post("/api/posts", data)
+      .then(() => {
+        toast("Post Created");
+      })
+      .catch((error) => {
+        toast(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <div>
+      <InputForm
+        id="body"
+        label="Body"
+        type="text"
+        disabled={loading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
 };
 
 export default Form;
