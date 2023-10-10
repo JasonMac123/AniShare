bde22c8756c7b4887a5e45b109b78088612e52f4
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-09 23:39:37 -0400

    Adds buttons for login and register and title of website

--- app/components/input/Form.tsx
+++ app/components/input/Form.tsx
@@ -12,6 +12,7 @@ import useLogin from "../hooks/useLoginModal";
 import useRegister from "../hooks/useRegisterModal";
 
 import InputForm from "./InputForm";
+import Button from "./Button";
 
 interface FormProps {
   placeholder: string;
@@ -55,7 +56,16 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
   };
 
   return (
-    <div>
+    <div className="border-b-[1px] border-neutral-800 px-5 py-2">
+      <div className="py-8">
+        <h1 className="text-white text-2xl text-center mb-4 font-bold">
+          Welcome to AniShare
+        </h1>
+        <div className="flex flex-row items-center justify-center gap-4">
+          <Button label="Login" onClick={loginModal.onOpen} />
+          <Button label="Register" onClick={registerModal.onOpen} />
+        </div>
+      </div>
       <InputForm
         id="body"
         label="Body"
