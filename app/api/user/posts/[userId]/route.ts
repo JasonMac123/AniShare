2bc81aa836def2ba83cd01b80914abb38afc9f4b
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-09 23:25:39 -0400

    Create route to get individual user posts and comments

---
+++ app/api/user/posts/[userId]/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse, NextRequest } from "next/server";
+
+import prisma from "../../../../prisma/prismadb";
+
+export async function GET(request: NextRequest, context: any) {
+  try {
+    const { userId } = context.params.userId;
+
+    const posts = await prisma.post.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      where: {
+        authorId: userId,
+      },
+      include: {
+        comment: true,
+      },
+    });
+    return NextResponse.json(posts);
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
