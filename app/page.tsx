ddb6a94eb2d9579783657daead25e2195d13e73e
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-09 23:40:19 -0400

    Import post creation form to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Form from "./components/input/Form";
 
 export default function Home() {
   return (
     <>
       <Header label="Home" disableBack />
+      <Form />
     </>
   );
 }
