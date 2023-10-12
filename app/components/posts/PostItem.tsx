60c6c8e17bd8f786af92ea343a0075aa724dbb0b
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-12 15:58:26 -0400

    Creates on click prop functions events for post item

--- app/components/posts/PostItem.tsx
+++ app/components/posts/PostItem.tsx
@@ -2,12 +2,33 @@
 
 import { SafePost } from "@/app/types";
 
+import { useCallback, useMemo } from "react";
+import { useRouter } from "next/navigation";
+import { formatDistanceToNowStrict } from "date-fns";
+
+import useLogin from "../hooks/useLoginModal";
+
 interface PostItemProps {
   userId: string;
   data: SafePost;
 }
 
 const PostItem: React.FC<PostItemProps> = ({ userId, data }) => {
+  const router = useRouter();
+  const loginModal = useLogin();
+
+  const redirectUserPage = useCallback(() => {
+    router.push(`/users/${data.authorId}`);
+  }, [router, data.authorId]);
+
+  const redirectPostPage = useCallback(() => {
+    router.push(`/posts/${data.id}`);
+  }, [router, data.id]);
+
+  const newDate = useMemo(() => {
+    return formatDistanceToNowStrict(new Date(data.createdAt));
+  }, [data.createdAt]);
+
   return <div></div>;
 };
 
