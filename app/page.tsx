04601d82d72328b5d0f88bff90dd8fe9907cdda0
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-12 15:30:59 -0400

    Creates api call to fetch posts and pass to post feed component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
+import axios from "axios";
+
 import Header from "./components/Header";
 import Form from "./components/input/Form";
+import PostFeed from "./components/posts/PostFeed";
+
 import getCurrentUser from "./functions/getCurrentUser";
 
 export default async function Home() {
   const user = await getCurrentUser();
+  const posts = await axios.get("http://localhost:3000/api/posts");
 
   return (
     <>
       <Header label="Home" disableBack />
       <Form user={user} />
+      <PostFeed posts={posts.data} />
     </>
   );
 }
