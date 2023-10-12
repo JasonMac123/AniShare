01ca586ef1f2acf7e4a235b00db7b87db485c009
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-12 15:43:37 -0400

    Creates post item component from iterating over posts retrieved

--- app/components/posts/PostFeed.tsx
+++ app/components/posts/PostFeed.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import { SafePost } from "@/app/types";
+import PostItem from "./PostItem";
 
 interface PostFeedProps {
-  posts?: SafePost[];
+  posts: SafePost[];
 }
 
 const PostFeed: React.FC<PostFeedProps> = ({ posts }) => {
-  return <div></div>;
+  return (
+    <>
+      {posts.map((post: SafePost) => {
+        <PostItem userId={post.authorId} key={post.id} data={post} />;
+      })}
+    </>
+  );
 };
 
 export default PostFeed;
---
+++ app/components/posts/PostItem.tsx
@@ -0,0 +1,5 @@
+const PostItem = () => {
+  return <div></div>;
+};
+
+export default PostItem;
