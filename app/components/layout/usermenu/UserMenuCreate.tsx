9c650f129ebe7b58501fc14f80cf90104f3e999b
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-13 16:01:45 -0400

    Fixes styling issues with background displaying without hovering cursor

--- app/components/layout/usermenu/UserMenuCreate.tsx
+++ app/components/layout/usermenu/UserMenuCreate.tsx
@@ -9,7 +9,7 @@ const UserMenuCreate = () => {
 
   return (
     <div onClick={() => router.push("/")}>
-      <div className="lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-isabelline text-isabelline hover:text-taupe hover:bg-opacity-50 transition cursor-pointer">
+      <div className="lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center hover:bg-isabelline text-isabelline hover:text-taupe hover:bg-opacity-50 transition cursor-pointer">
         <BiCommentAdd size={28} />
       </div>
       <div className="hidden lg:flex p-4 gap-4 rounded-full items-center hover:bg-isabelline text-isabelline hover:text-taupe cursor-pointer">
