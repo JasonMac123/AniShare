7d0a9732fe4998edf45d85c34e820a896dd95007
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-14 22:31:23 -0400

    Changes layout of post item

--- app/components/posts/PostItem.tsx
+++ app/components/posts/PostItem.tsx
@@ -43,52 +43,46 @@ const PostItem: React.FC<PostItemProps> = ({ userId, data, user }) => {
   return (
     <div
       onClick={redirectPostPage}
-      className="border-b-[1px] border-neutral-800 hover:bg-neutral-900 cursor-pointer transition p-4 pr-8"
+      className="border-b-[1px] border-neutral-800 hover:bg-neutral-900 cursor-pointer transition p-8 py-4 space-y-2"
     >
-      <div className="flex flex-row items-start gap-3">
+      <div className="flex flex-row items-center gap-2">
         <Avatar userId={data.authorId} userImage={data.author.profileImage} />
-        <div className="w-full">
-          <div className="flex flex-row items-center gap-2">
-            <p
-              className="text-white font-semibold cursor-pointer hover:underline"
-              onClick={redirectUserPage}
-            >
-              @{data.author.username}
-            </p>
-            <span className="text-neutral-100 text-sm">
-              {dateSincePost} ago
-            </span>
+        <p
+          className="text-white font-semibold cursor-pointer hover:underline"
+          onClick={redirectUserPage}
+        >
+          @{data.author.username}
+        </p>
+        <span className="text-neutral-100 text-sm">{dateSincePost} ago</span>
+      </div>
+      {data.image ? (
+        <div className="flex flex-col w-full">
+          <div className="w-full">
+            <Image
+              alt="image associated with the post"
+              src={data.image}
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-full min-w-[150px] min-h-[150px]"
+            />
           </div>
-          {data.image ? (
-            <div className="flex flex-col w-full">
-              <div className="w-full">
-                <Image
-                  alt="image associated with the post"
-                  src={data.image}
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                  className="w-full h-full min-w-[150px] min-h-[150px]"
-                />
-              </div>
-              <div className="text-white mt-1">{data.body}</div>
-            </div>
-          ) : (
-            <div className="text-white mt-1">{data.body}</div>
-          )}
-          <div className="flex flex-row items-center mt-3 gap-10">
-            <div className="flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-white">
-              <AiOutlineMessage size={20} />
-              <p>{data.comment?.length || 0}</p>
-            </div>
-            <div
-              className="flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500"
-              onClick={onLike}
-            >
-              <AiOutlineHeart size={20} />
-              <p>{data.comment?.length || 0}</p>
-            </div>
-          </div>
+          <div className="text-white mt-1">{data.body}</div>
+        </div>
+      ) : (
+        <div className="text-white mt-1">{data.body}</div>
+      )}
+      <div className="flex flex-row items-center mt-3 gap-10">
+        <div className="flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-white">
+          <AiOutlineMessage size={20} />
+          <p>{data.comment?.length || 0}</p>
+        </div>
+        <div
+          className="flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500"
+          onClick={onLike}
+        >
+          <AiOutlineHeart size={20} />
+          <p>{data.comment?.length || 0}</p>
         </div>
       </div>
     </div>
