2982b26f0f47466de15c5cafcd094a0a0873feaa
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-15 23:08:32 -0400

    Creates route for removing users from follow list

--- app/api/follow/route.ts
+++ app/api/follow/route.ts
@@ -39,3 +39,42 @@ export async function POST(request: NextRequest) {
     throw new Error(error);
   }
 }
+
+export async function DELETE(request: NextRequest) {
+  try {
+    const { userId } = await request.json();
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) {
+      throw new Error("Invalid Id");
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) {
+      throw new Error("Invalid following user Id");
+    }
+
+    let updatedFollowingIds = [...currentUser.following];
+    updatedFollowingIds = updatedFollowingIds.filter(
+      (user) => user.id !== currentUser.id
+    );
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        following: updatedFollowingIds,
+      },
+    });
+
+    return NextResponse.json(updatedUser);
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
