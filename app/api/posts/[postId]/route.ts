b5d0056f222b1396d91ce32ab7ff00448da3e853
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-17 19:14:49 -0400

    Create post api call to fetch post and comments associated

---
+++ app/api/posts/[postId]/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+import prisma from "@/app/prisma/prismadb";
+
+interface getPostDetailsParams {
+  postId?: string;
+}
+
+export async function GET(
+  request: Request,
+  { params }: { params: getPostDetailsParams }
+) {
+  try {
+    const { postId } = params;
+
+    const post = await prisma.post.findUnique({
+      where: {
+        id: postId,
+      },
+      include: {
+        author: true,
+        comment: true,
+      },
+    });
+
+    return NextResponse.json(post);
+  } catch (e: any) {
+    throw new Error(e);
+  }
+}
