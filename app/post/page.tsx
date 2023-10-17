6c70da55bc6b3070faf6f4d1a8a5c721b6c5ed14
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-17 19:18:18 -0400

    Retrieves post details and sends to client

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,4 +1,14 @@
-const Page = () => {
+import axios from "axios";
+
+interface PostPageProps {
+  postId: string;
+}
+
+const Page = async ({ params }: { params: PostPageProps }) => {
+  const postData = await axios.get(
+    `http://localhost:3000/api/posts/${params.postId}`
+  );
+
   return <div></div>;
 };
 
