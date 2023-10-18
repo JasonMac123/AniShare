2cf973c43c83ffbdff6f1adb90fad271beb72391
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-18 15:14:19 -0400

    Creates individual post page with header and post item component

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+import getCurrentUser from "../functions/getCurrentUser";
+
+import Header from "../components/Header";
+import PostItem from "../components/posts/PostItem";
+import Form from "../components/input/Form";
+
 interface PostPageProps {
   postId: string;
 }
@@ -8,8 +14,19 @@ const Page = async ({ params }: { params: PostPageProps }) => {
   const postData = await axios.get(
     `http://localhost:3000/api/posts/${params.postId}`
   );
+  const currentUser = await getCurrentUser();
 
-  return <div></div>;
+  if (!postData) {
+    return <div className="flex justify-center items-center h-full"></div>;
+  }
+
+  return (
+    <>
+      <Header label="Tweet" />
+      <PostItem data={postData.data} user={currentUser} />
+      <Form isComment user={currentUser} postId={postData.data.id} />
+    </>
+  );
 };
 
 export default Page;
