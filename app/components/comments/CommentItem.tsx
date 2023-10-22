ccb8b696692f3d0074e94e524695c4eed082f17f
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-21 20:53:28 -0400

    Adds prop types to comment item

--- app/components/comments/CommentItem.tsx
+++ app/components/comments/CommentItem.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-const CommentItem = () => {
+import { SafeComment } from "@/app/types";
+
+interface CommentItemProp {
+  data: SafeComment;
+}
+
+const CommentItem: React.FC<CommentItemProp> = ({ data }) => {
   return <div></div>;
 };
 
