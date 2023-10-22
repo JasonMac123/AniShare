eeffb8f54af55fc8d0f57bca0c21d2235fa8ed9f
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-21 21:13:00 -0400

    Creates user redirect and date functions

--- app/components/comments/CommentItem.tsx
+++ app/components/comments/CommentItem.tsx
@@ -1,12 +1,36 @@
 "use client";
 
+import { useCallback, useMemo } from "react";
+import { useRouter } from "next/navigation";
+
 import { SafeComment } from "@/app/types";
+import { formatDistanceToNowStrict } from "date-fns";
 
 interface CommentItemProp {
   data: SafeComment;
 }
 
 const CommentItem: React.FC<CommentItemProp> = ({ data }) => {
+  const router = useRouter();
+
+  const redirectUserPage = useCallback(
+    (event: any) => {
+      event.stopPropagation();
+
+      router.push(`/users/${data.user.id}`);
+      return;
+    },
+    [router, data]
+  );
+
+  const newDate = useMemo(() => {
+    if (!data.createdAt) {
+      return null;
+    }
+
+    return formatDistanceToNowStrict(new Date(data.createdAt));
+  }, [data.createdAt]);
+
   return <div></div>;
 };
 
