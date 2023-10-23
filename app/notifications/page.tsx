e2cf864af23b3eb07171299fa2ce9c27ad752008
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-22 23:31:05 -0400

    Creates route protection function for notifications for non users

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -1,4 +1,25 @@
+import { NextPageContext } from "next";
 import Header from "../components/Header";
+import getCurrentUser from "../functions/getCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const currentUser = getCurrentUser();
+
+  if (!currentUser) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      currentUser,
+    },
+  };
+}
 
 const Notifications = () => {
   return (
