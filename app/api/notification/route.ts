439be15575e48f6bc4f25d09b949d6c2860e2d4f
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-22 23:26:27 -0400

    Creates api notification route to display user notifications

---
+++ app/api/notification/route.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest } from "next";
+
+import prisma from "@/app/prisma/prismadb";
+import { NextResponse } from "next/server";
+
+export async function GET(req: NextApiRequest) {
+  try {
+    const userId = req.query;
+
+    if (!userId) {
+      throw new Error("Invalid Id");
+    }
+
+    const notifications = await prisma.notification.findMany({
+      where: {
+        userId,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        hasNotification: false,
+      },
+    });
+
+    return NextResponse.json(notifications);
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
