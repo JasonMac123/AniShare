7af3f51b1b06ac3296af6a2a6c5a71ae924811b8
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-22 23:29:32 -0400

    Create notification page to show messages

---
+++ app/notifications/page.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+const Notifications = () => {
+  return (
+    <>
+      <Header label="Notifications" />
+    </>
+  );
+};
+
+export default Notifications;
