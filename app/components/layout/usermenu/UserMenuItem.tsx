118ce17f141d2877b6c0958a22c20f329e6859a1
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-26 16:12:44 -0400

    Styles the alert notification for user menu items

--- app/components/layout/usermenu/UserMenuItem.tsx
+++ app/components/layout/usermenu/UserMenuItem.tsx
@@ -41,18 +41,21 @@ const UserMenuItem: React.FC<UserMenuItemProps> = ({
   }, [router, onClick, href, currentUser]);
 
   return (
-    <div onClick={handleClick} className="flex flex-row items-center">
+    <div onClick={handleClick} className="flex flex-row items-center relative">
       <div className="rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-isabelline text-isabelline hover:text-taupe cursor-pointer lg:hidden">
         <Icon size={28} />
         {alert ? (
-          <BsDot className="text-melon absolute -top-4 left-0" size={70} />
+          <BsDot
+            className="text-melon absolute top-[2px] left-[2px]"
+            size={70}
+          />
         ) : null}
       </div>
       <div className="hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-isabelline text-isabelline hover:text-taupe cursor-pointer">
         <Icon size={24} />
         <p className="hidden lg:block text-xl">{label}</p>
         {alert ? (
-          <BsDot className="text-melon absolute -top-4 left-0" size={70} />
+          <BsDot className="text-melon absolute top-1 right-40" size={70} />
         ) : null}
       </div>
     </div>
