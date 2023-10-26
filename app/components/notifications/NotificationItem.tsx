7ae79647eeea7a122c38ab1be18e9fb04a573d7f
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-26 15:21:24 -0400

    Adds onClick function to redirect user from notification to link

--- app/components/notifications/NotificationItem.tsx
+++ app/components/notifications/NotificationItem.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Notification } from "@prisma/client";
+import { useRouter } from "next/navigation";
 import { BiLogoInstagram } from "react-icons/bi";
 
 interface NotificationItemProps {
@@ -8,8 +9,23 @@ interface NotificationItemProps {
 }
 
 const NotificationItem: React.FC<NotificationItemProps> = ({ data }) => {
+  const router = useRouter();
+
+  const onClick = () => {
+    if (data.type === "Post") {
+      router.push(`/post/${data.reference}`);
+    }
+
+    if (data.type === "User") {
+      router.push(`/users/${data.reference}`);
+    }
+  };
+
   return (
-    <div className="flex flex-row items-center p-6 gap-4 border-b-[1px] border-neutral-800">
+    <div
+      className="flex flex-row items-center p-6 gap-4 border-b-[1px] border-neutral-800"
+      onClick={onClick}
+    >
       <BiLogoInstagram size={40} color="white" />
       <p className="text-white">{data.body}</p>
     </div>
