81706f9df5e566d77ee6171077a4528939c5a751
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-27 19:38:10 -0400

    Adds date for notification

--- app/components/notifications/NotificationItem.tsx
+++ app/components/notifications/NotificationItem.tsx
@@ -1,15 +1,19 @@
 "use client";
 
-import { Notification } from "@prisma/client";
+import { SafeNotification } from "@/app/types";
+import { formatDistanceToNowStrict } from "date-fns";
 import { useRouter } from "next/navigation";
+import { useMemo, useEffect, useState } from "react";
 import { BiLogoInstagram } from "react-icons/bi";
 
 interface NotificationItemProps {
-  data: Notification;
+  data: SafeNotification;
 }
 
 const NotificationItem: React.FC<NotificationItemProps> = ({ data }) => {
   const router = useRouter();
+
+  const [distanceToFromNow, setDistanceToFromNow] = useState(data.createdAt);
 
   const onClick = () => {
     if (data.type === "Post") {
@@ -21,13 +25,23 @@ const NotificationItem: React.FC<NotificationItemProps> = ({ data }) => {
     }
   };
 
+  const newDate = useMemo(() => {
+    return formatDistanceToNowStrict(new Date(data.createdAt));
+  }, [data.createdAt]);
+
+  useEffect(() => {
+    setDistanceToFromNow(newDate);
+  });
+
   return (
     <div
       className="flex flex-row items-center p-6 gap-4 border-b-[1px] border-neutral-800 hover:neutral-900 cusrsor-pointer transition"
       onClick={onClick}
     >
       <BiLogoInstagram size={40} color="white" />
-      <p className="text-white">{data.body}</p>
+      <div>
+        <p className="text-white">{data.body}</p>
+      </div>
     </div>
   );
 };
