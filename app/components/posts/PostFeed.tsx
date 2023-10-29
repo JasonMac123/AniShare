142330a392fb85559546dcbc4b30ee7d834df57d
JasonSnow123 <m.jason.2018@gmail.com>
2023-10-28 20:15:57 -0400

    Adds error message styling if there are no posts render

--- app/components/posts/PostFeed.tsx
+++ app/components/posts/PostFeed.tsx
@@ -9,6 +9,12 @@ interface PostFeedProps {
 }
 
 const PostFeed: React.FC<PostFeedProps> = ({ posts, user }) => {
+  if (posts.length === 0) {
+    return (
+      <div className="text-white text-center p-6 text-xl">No notifications</div>
+    );
+  }
+
   return (
     <>
       {posts.map((post: SafePost) => {
